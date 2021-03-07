1f3e0bb2b545cb461c1dce1b303fa90c65d89f2a
Dylan Schroder <dylan.schroder31@gmail.com>
2021-03-07 14:27:31 -0600

    Finish script

--- converter.py
+++ converter.py
@@ -5,40 +5,43 @@
 # Proceeds      => USD Total (inclusive of fees)
 # Timestamp	| Transaction Type	| Asset	| Quantity Transacted | USD Spot Price at Transaction | USD Subtotal |	USD Total (inclusive of fees) |	USD Fees |	Notes
 import csv
+import tkinter as tk
+from tkinter import filedialog as fd
 from datetime import datetime
+import tkinter.messagebox
+import glob
+import os
+import webbrowser
 
 
-    
 def getCurrentRowAsDict(row):
     # Skip if its not a Buy or Sell transaction or if this is the header row
     if row[1] not in ['Buy', 'Sell'] or row[0] == 'Timestamp':
         return None
     return {
-            'Timestamp': row[0],
-            'Transaction Type': row[1],
-            'Asset': row[2],
-            # 'Quantity Transacted': row[3],
-            'USD Spot Price at Transaction': row[4],
-            # 'USD Subtotal': row[5],
-            'USD Total': row[6],
-            # 'USD Fees': row[7],
-            # 'Notes': row[8]
-        }
+        'Timestamp': row[0],
+        'Transaction Type': row[1],
+        'Asset': row[2],
+        # 'Quantity Transacted': row[3],
+        'USD Spot Price at Transaction': row[4],
+        # 'USD Subtotal': row[5],
+        'USD Total': row[6],
+        # 'USD Fees': row[7],
+        # 'Notes': row[8]
+    }
+
 
 def createNewRow(transactionType, assetName, usdPriceAtTransaction, proceeds, year, month, day):
-    updatedRow={}
-    # Column Interpretations go here
-    updatedRow['Currency Name'] = assetName
-    updatedRow['Cost Basis'] = usdPriceAtTransaction
-    updatedRow['Proceeds'] = proceeds
-    if transactionType == 'Buy':
-        updatedRow['Purchase Date'] = "{}/{}/20".format(month, day)
-        updatedRow['Date Sold'] = ''
-    elif transactionType == 'Sell':
-        updatedRow['Purchase Date'] = ''
-        updatedRow['Date Sold'] = "{}/{}/20".format(month, day)
+    updatedRow = {
+        'Purchase Date': "{}/{}/20".format(month, day) if transactionType == 'Buy' else '',
+        'Date Sold': "{}/{}/20".format(month, day) if transactionType == 'Sell' else '',
+        'Currency Name': assetName,
+        'Cost Basis': usdPriceAtTransaction,
+        'Proceeds': proceeds,
+    }
     print(updatedRow)
     return updatedRow
+
 
 def convertToNewFormat(sourcefile):
     newRows = []
@@ -55,11 +58,13 @@ def convertToNewFormat(sourcefile):
             timestamp = currentRow['Timestamp'].split('T')[0].split('-')
 
             # Get year month day from timestamp
-            year = 20 # Abbreviated. NOTE: If this is used in the future for 2021 you must update this line
+            year = 20  # Abbreviated. NOTE: If this is used in the future for 2021 you must update this line
             month = timestamp[1]
             day = timestamp[2]
-            newRows.append(createNewRow(transactionType, assetName, usdPriceAtTransaction, proceeds, year, month, day))
+            newRows.append(createNewRow(transactionType, assetName,
+                                        usdPriceAtTransaction, proceeds, year, month, day))
     return newRows
+
 
 def saveToNewFormat(newRows):
     csv_columns = newRows[0].keys()
@@ -71,10 +76,34 @@ def saveToNewFormat(newRows):
             writer.writeheader()
             for data in newRows:
                 writer.writerow(data)
+        return csv_file
     except IOError as e:
         print("I/O error", e)
+
+def selectCSV():
+    name = fd.askopenfilename()
+    if '.csv' not in name:
+        tkinter.messagebox.showinfo(
+            'ERROR', 'ERROR: File must be of .csv format')
+        raise Exception("Must be a csv file")
+    print(name)
+    newRows = convertToNewFormat(name)
+    newFilename = saveToNewFormat(newRows)
+    tkinter.messagebox.showinfo('Success', f'Success! File was saved to {newFilename}')
+
+def googleHowTo():
+    webbrowser.open('https://www.google.com/search?q=coinbase+how+to+download+csv')
+
 def main():
-    newRows = convertToNewFormat('coinbase.csv')
-    saveToNewFormat(newRows)
+    root = tkinter.Tk()
+    root.title('Coinbase CSV Converter')
+    root.geometry('500x200')
+    root.resizable(False, False)   
+    tkinter.Button(root, text='Select CSV file to open', command=selectCSV).pack()
+    tkinter.Button(root, text='How to retrieve your CSV from Coinbase', command=googleHowTo).pack()
+    tkinter.Button(root, text='Exit', command=exit).pack()
+    tk.mainloop()
+   
+
 
 main()
